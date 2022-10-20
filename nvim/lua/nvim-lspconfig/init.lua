894983141b5310dd491dcf021479db171622e8ea
k33nice <k33nice@gmail.com>
2022-10-21 00:58:44 +0300

    feat(vim): Add diagnostic config

--- nvim/lua/nvim-lspconfig/init.lua
+++ nvim/lua/nvim-lspconfig/init.lua
@@ -135,6 +135,15 @@ mason_lspconfig.setup_handlers({
     end
 })
 
+vim.diagnostic.config {
+  float = {
+    border = "rounded",
+    source = true,
+    header = "",
+    prefix = "",
+  },
+}
+
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
         virtual_text = false,
